6c99a0e8c398e9b4fccbaeb19dc537b8fd022b4d
madiha <madihashaikh2245@gmail.com>
2024-06-24 21:53:25 +0530

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
+=======
+import './index.css';
+>>>>>>> 560ad97 (Initialize project using Create React App)
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
